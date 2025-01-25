93cceca24aef52d9a7c9359b19f4cf0a2fbd75ff
AlvarowJS <alvaruwiiu@hotmail.com>
2025-01-25 02:06:54 -0500

    authentication implementation

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\LoginResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|email',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The credentials are incorrect'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json([
+            'data' => new LoginResource($user),
+            'token' => $user->createToken('token')->plainTextToken            
+        ], Response::HTTP_OK);
+    }
+    public function login(Request $request) {}
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ReportController as Report;
+use App\Http\Controllers\Api\LoginController as Login;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,9 @@ use App\Http\Controllers\Api\ReportController as Report;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/generate-report',         [Report::class, 'generateReport']);
-Route::get('/get-report/{report_id}',  [Report::class, 'getReport']);
-Route::get('/list-reports',            [Report::class, 'listReport']);
+Route::post('/login',                      [Login::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/generate-report',        [Report::class, 'generateReport']);
+    Route::get('/get-report/{report_id}',  [Report::class, 'getReport']);
+    Route::get('/list-reports',            [Report::class, 'listReport']);
+});
